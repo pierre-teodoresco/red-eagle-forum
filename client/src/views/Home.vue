7bbb41d860b4ee79b20a2a3e3eac3964848e754d
pierre-teodoresco <p.teodoresco@gmail.com>
2023-10-13 19:47:01 +0200

    frontend routes

---
+++ client/src/views/Home.vue
@@ -0,0 +1,14 @@
+<!-- src/views/Home.vue -->
+<template>
+    <div>
+      <h1>Home Page</h1>
+      <router-link to="/register">Go to Register</router-link>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    // Component logic here
+  };
+  </script>
+  
---
+++ client/src/views/Register.vue
@@ -0,0 +1,14 @@
+<!-- src/views/Register.vue -->
+<template>
+    <div>
+      <h1>Register Page</h1>
+      <!-- Your registration form goes here -->
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    // Component logic here
+  };
+  </script>
+  
