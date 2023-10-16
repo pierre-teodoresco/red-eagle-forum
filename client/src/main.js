b4f3a10f2750c018fe53defa9ea781c00352776c
pierre-teodoresco <p.teodoresco@gmail.com>
2023-10-16 13:38:23 +0200

    style login et register

--- client/src/main.js
+++ client/src/main.js
@@ -2,5 +2,6 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router/routes.js';
+import './style.css'
 
 createApp(App).use(router).mount('#app');
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,9 +1,10 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
-  theme: {
-    extend: {},
-  },
+module.exports = {
+  mode: 'jit',
+  content: [
+    './index.html', 
+    './src/**/*.{vue,js,ts,jsx,tsx}'
+  ],
+  theme: {},
+  variants: {},
   plugins: [],
-}
-
+};
